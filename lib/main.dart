956353ebb189dab223d21d5ac4da860ef53538b6
shenoyganesprasad <shenoyganeshprasad@gmail.com>
2019-12-28 21:19:48 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'homePage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quotes',
+      home: HomePage(),
+    );
+  }
+}
